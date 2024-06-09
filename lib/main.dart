b7763a2e6db52e753598fe0c38b11c0bd6eed58d
Wreck-X <hemanthkrishnakumar@gmail.com>
2024-06-09 22:03:08 +0530

    test

---
+++ lib/main.dart
@@ -0,0 +1,221 @@
+import 'package:flutter/material.dart';
+
+import 'package:jitsi_meet_flutter_sdk/jitsi_meet_flutter_sdk.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool audioMuted = true;
+  bool videoMuted = true;
+  bool screenShareOn = false;
+  List<String> participants = [];
+  final _jitsiMeetPlugin = JitsiMeet();
+
+  join() async {
+    var options = JitsiMeetConferenceOptions(
+      serverURL: "http://testmeet.biscuitbobby.eu.org",
+      room: "test",
+      configOverrides: {
+        "startWithAudioMuted": true,
+        "startWithVideoMuted": true,
+      },
+      featureFlags: {
+        FeatureFlags.addPeopleEnabled: false,
+        FeatureFlags.welcomePageEnabled: false,
+        FeatureFlags.preJoinPageEnabled: false,
+        FeatureFlags.unsafeRoomWarningEnabled: false,
+        FeatureFlags.resolution: FeatureFlagVideoResolutions.resolution720p,
+        FeatureFlags.audioFocusDisabled: false,
+        FeatureFlags.audioMuteButtonEnabled: true,
+        FeatureFlags.audioOnlyButtonEnabled: false,
+        FeatureFlags.calenderEnabled: false,
+        FeatureFlags.callIntegrationEnabled: true,
+        FeatureFlags.carModeEnabled: true,
+        FeatureFlags.closeCaptionsEnabled: true,
+        FeatureFlags.conferenceTimerEnabled: true,
+        FeatureFlags.chatEnabled: true,
+        FeatureFlags.filmstripEnabled: true,
+        FeatureFlags.fullScreenEnabled: true,
+        FeatureFlags.helpButtonEnabled: true,
+        FeatureFlags.inviteEnabled: false,
+        FeatureFlags.androidScreenSharingEnabled: true,
+        FeatureFlags.speakerStatsEnabled: true,
+        FeatureFlags.kickOutEnabled: false,
+        FeatureFlags.liveStreamingEnabled: true,
+        FeatureFlags.lobbyModeEnabled: false,
+        FeatureFlags.meetingNameEnabled: false,
+        FeatureFlags.meetingPasswordEnabled: true,
+        FeatureFlags.notificationEnabled: true,
+        FeatureFlags.overflowMenuEnabled: false,
+        FeatureFlags.pipEnabled: true,
+        FeatureFlags.pipWhileScreenSharingEnabled: true,
+        FeatureFlags.preJoinPageHideDisplayName: true,
+        FeatureFlags.raiseHandEnabled: true,
+        FeatureFlags.reactionsEnabled: false,
+        FeatureFlags.recordingEnabled: true,
+        FeatureFlags.replaceParticipant: false,
+        FeatureFlags.securityOptionEnabled: false,
+        FeatureFlags.serverUrlChangeEnabled: true,
+        FeatureFlags.settingsEnabled: false,
+        FeatureFlags.tileViewEnabled: true,
+        FeatureFlags.videoMuteEnabled: false,
+        FeatureFlags.videoShareEnabled: true,
+        FeatureFlags.toolboxEnabled: true,
+        FeatureFlags.iosRecordingEnabled: true,
+        FeatureFlags.iosScreenSharingEnabled: true,
+        FeatureFlags.toolboxAlwaysVisible: true,
+      },
+      userInfo: JitsiMeetUserInfo(
+          displayName: "The real lemon",
+          email: "hemanthkrishnakumarclt@gmail.com",
+          avatar:
+              "https://avatars.githubusercontent.com/u/57035818?s=400&u=02572f10fe61bca6fc20426548f3920d53f79693&v=4"),
+    );
+
+    var listener = JitsiMeetEventListener(
+      conferenceJoined: (url) {
+        debugPrint("conferenceJoined: url: $url");
+      },
+      conferenceTerminated: (url, error) {
+        debugPrint("conferenceTerminated: url: $url, error: $error");
+      },
+      conferenceWillJoin: (url) {
+        debugPrint("conferenceWillJoin: url: $url");
+      },
+      participantJoined: (email, name, role, participantId) {
+        debugPrint(
+          "participantJoined: email: $email, name: $name, role: $role, "
+          "participantId: $participantId",
+        );
+        participants.add(participantId!);
+      },
+      participantLeft: (participantId) {
+        debugPrint("participantLeft: participantId: $participantId");
+      },
+      audioMutedChanged: (muted) {
+        debugPrint("audioMutedChanged: isMuted: $muted");
+      },
+      videoMutedChanged: (muted) {
+        debugPrint("videoMutedChanged: isMuted: $muted");
+      },
+      endpointTextMessageReceived: (senderId, message) {
+        debugPrint(
+            "endpointTextMessageReceived: senderId: $senderId, message: $message");
+      },
+      screenShareToggled: (participantId, sharing) {
+        debugPrint(
+          "screenShareToggled: participantId: $participantId, "
+          "isSharing: $sharing",
+        );
+      },
+      chatMessageReceived: (senderId, message, isPrivate, timestamp) {
+        debugPrint(
+          "chatMessageReceived: senderId: $senderId, message: $message, "
+          "isPrivate: $isPrivate, timestamp: $timestamp",
+        );
+      },
+      chatToggled: (isOpen) => debugPrint("chatToggled: isOpen: $isOpen"),
+      participantsInfoRetrieved: (participantsInfo) {
+        debugPrint(
+            "participantsInfoRetrieved: participantsInfo: $participantsInfo, ");
+      },
+      readyToClose: () {
+        debugPrint("readyToClose");
+      },
+    );
+    await _jitsiMeetPlugin.join(options, listener);
+  }
+
+  hangUp() async {
+    await _jitsiMeetPlugin.hangUp();
+  }
+
+  setAudioMuted(bool? muted) async {
+    var a = await _jitsiMeetPlugin.setAudioMuted(muted!);
+    debugPrint("$a");
+    setState(() {
+      audioMuted = muted;
+    });
+  }
+
+  setVideoMuted(bool? muted) async {
+    var a = await _jitsiMeetPlugin.setVideoMuted(muted!);
+    debugPrint("$a");
+    setState(() {
+      videoMuted = muted;
+    });
+  }
+
+  sendEndpointTextMessage() async {
+    var a = await _jitsiMeetPlugin.sendEndpointTextMessage(message: "HEY");
+    debugPrint("$a");
+
+    for (var p in participants) {
+      var b =
+          await _jitsiMeetPlugin.sendEndpointTextMessage(to: p, message: "HEY");
+      debugPrint("$b");
+    }
+  }
+
+  toggleScreenShare(bool? enabled) async {
+    await _jitsiMeetPlugin.toggleScreenShare(enabled!);
+
+    setState(() {
+      screenShareOn = enabled;
+    });
+  }
+
+  openChat() async {
+    await _jitsiMeetPlugin.openChat();
+  }
+
+  sendChatMessage() async {
+    var a = await _jitsiMeetPlugin.sendChatMessage(message: "HEY1");
+    debugPrint("$a");
+
+    for (var p in participants) {
+      a = await _jitsiMeetPlugin.sendChatMessage(to: p, message: "HEY2");
+      debugPrint("$a");
+    }
+  }
+
+  closeChat() async {
+    await _jitsiMeetPlugin.closeChat();
+  }
+
+  retrieveParticipantsInfo() async {
+    var a = await _jitsiMeetPlugin.retrieveParticipantsInfo();
+    debugPrint("$a");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Plugin example app'),
+          ),
+          body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Center(
+                    child: TextButton(
+                      onPressed: join,
+                      child: const Text("Join"),
+                    ),
+                  ),
+                ]),
+          )),
+    );
+  }
+}
